599d17dd819fc12e34402b0e497d2b0e897e260a
Vyltt-25 <vyltt.work@gmail.com>
2023-09-30 01:19:46 +0700

    attack mobs

---
+++ MenuProject/Assets/Mob/Skeleton/Skeleton_Script/MobController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobController : MonoBehaviour
+{
+    public Animator animator;
+    public Transform player;
+    public float walkDistance = 10f;
+    public float walkSpeed = 2f;
+    public float attackDistance = 2f;
+
+    private bool rebornToIdleComplete;
+    private bool canMove = true;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        if (direction.x > 0)
+        {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1);
+        }
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1.5f, 1.5f, 1);
+        }
+
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance < walkDistance && !rebornToIdleComplete)
+        {
+            if (OnRebornToIdleComplete())
+            {
+                if (canMove)
+                {
+                    animator.SetBool("IsWalking", true);
+                    transform.Translate(direction * walkSpeed * Time.deltaTime, Space.World);
+                }
+            }
+            rebornToIdleComplete = false;
+            if (distance < attackDistance)
+            {
+                canMove = false;
+                animator.SetBool("IsWalking", false);
+                animator.SetTrigger("ToAttack");
+            }
+            else
+            {
+                canMove = true;
+                animator.SetBool("IsWalking", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+
+    public bool OnRebornToIdleComplete()
+    {
+        animator.SetTrigger("IntroToReborn");
+        animator.SetTrigger("RebornToIdle");
+        return rebornToIdleComplete = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy die");
+        animator.SetBool("IsDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
--- MenuProject/Assets/Player/Script/Player/PlayerController.cs
+++ MenuProject/Assets/Player/Script/Player/PlayerController.cs
@@ -8,20 +8,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float walkSpeed = 9f;
     [SerializeField]
-    private float jumpForce = 850;
+    private float jumpForce = 10f;
     [SerializeField]
-    private float attackDelay = 0.3f;
+    private float attackDelay = 0.7f;
 
+    public Transform attackPoint;
     private Animator animator;
     private Rigidbody2D rb2d;
     private bool isJumpPressed;
     private bool isAttackPressed;
     private bool isAttacking;
-    private bool isGrounded;
+    public bool isGrounded;
+    private float xAxis;
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+    public LayerMask enemyLayers;
 
     const string PLAYER_IDLE = "Player_Idle";
     const string PLAYER_RUN = "Player_Run";
     const string PLAYER_JUMP = "Player_Jumping";
+    const string PLAYER_Falling = "Player_Falling";
     const string PLAYER_ATTACK = "Player_Attack";
 
     void Start()
@@ -32,52 +38,86 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float xAxis = Input.GetAxisRaw("Horizontal");
-        isJumpPressed = Input.GetKeyDown(KeyCode.Space);
-        isAttackPressed = Input.GetKeyDown(KeyCode.RightControl);
+        xAxis = Input.GetAxisRaw("Horizontal");
 
-        if (xAxis != 0)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.localScale = new Vector2(Mathf.Sign(xAxis), 1);
-        }
-
-        if (isJumpPressed)
-        {
-            rb2d.AddForce(new Vector2(0, jumpForce));
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             ChangeAnimationState(PLAYER_JUMP);
-            Debug.Log("Jumping");
-            isJumpPressed = false;
+            isJumpPressed = true;
         }
-        // else if (isAttackPressed)
-        // {
-        //     ChangeAnimationState(PLAYER_ATTACK);
-        //     isAttacking = true;
-        //     Invoke("AttackComplete", attackDelay);
-        // }
-        else if (Mathf.Abs(xAxis) > 0.1f)
-        {
-            ChangeAnimationState(PLAYER_RUN);
-            Debug.Log("Running");
-        }
-        // else
-        // {
-        //     ChangeAnimationState(PLAYER_IDLE);
-        //     Debug.Log("Idle");
 
-        // }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            isAttackPressed = true;
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        float xAxis = Input.GetAxisRaw("Horizontal");
-        Vector2 vel = new Vector2(xAxis * walkSpeed, rb2d.velocity.y);
-        rb2d.velocity = vel;
+        Vector2 vel = new Vector2(0, rb2d.velocity.y);
 
-        // check isGround
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
-        Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.1f, Color.red);
-        isGrounded = (hit.collider != null);
-        Debug.Log(isGrounded);
+        if (xAxis < 0)
+        {
+            // if (!isAttacking)
+            // {
+            vel.x = -walkSpeed;
+            transform.localScale = new Vector2(-1, 1);
+            // }
+        }
+        else if (xAxis > 0)
+        {
+            // if (!isAttacking)
+            // {
+            vel.x = walkSpeed;
+            transform.localScale = new Vector2(1, 1);
+            // }
+        }
+        else
+        {
+            vel.x = 0;
+
+        }
+
+        if (isGrounded && !isAttacking)
+        {
+            if (xAxis != 0)
+            {
+                ChangeAnimationState(PLAYER_RUN);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
+
+        if (isJumpPressed && isGrounded)
+        {
+            ChangeAnimationState(PLAYER_JUMP);
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumpPressed = false;
+        }
+
+        if (isAttackPressed)
+        {
+            isAttackPressed = false;
+            if (!isAttacking)
+            {
+                isAttacking = true;
+                if (isGrounded)
+                {
+                    ChangeAnimationState(PLAYER_ATTACK);
+                    Collider2D[] hitEnimies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+                    foreach (Collider2D enemy in hitEnimies)
+                    {
+                        enemy.GetComponent<MobController>().TakeDamage(attackDamage);
+                    }
+                }
+                Invoke("AttackComplete", attackDelay);
+            }
+        }
+
+        rb2d.velocity = vel;
     }
 
     void AttackComplete()
@@ -91,251 +131,25 @@ public class PlayerController : MonoBehaviour
         animator.Play(newAnimation);
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (attackPoint == null) return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 }
-
-
-
-
-
-
-// public class PlayerController : MonoBehaviour
-// {
-//     [SerializeField]
-//     private float walkSpeed = 9f;
-//     [SerializeField]
-//     private float jumpForce = 850;
-//     [SerializeField]
-//     private float attackDelay = 0.3f;
-
-//     private Animator animator;
-//     private Rigidbody2D rb2d;
-//     private bool isJumpPressed;
-//     private bool isAttackPressed;
-//     private bool isAttacking;
-
-//     const string PLAYER_IDLE = "Player_Idle";
-//     const string PLAYER_RUN = "Player_Run";
-//     const string PLAYER_JUMP = "Player_Jumping";
-//     const string PLAYER_ATTACK = "Player_Attack";
-
-//     void Start()
-//     {
-//         rb2d = GetComponent<Rigidbody2D>();
-//         animator = GetComponent<Animator>();
-//     }
-
-//     void Update()
-//     {
-//         float xAxis = Input.GetAxisRaw("Horizontal");
-//         isJumpPressed = Input.GetKeyDown(KeyCode.Space);
-//         isAttackPressed = Input.GetKeyDown(KeyCode.RightControl);
-
-//         if (xAxis != 0)
-//         {
-//             transform.localScale = new Vector2(Mathf.Sign(xAxis), 1);
-//         }
-
-//         if (isJumpPressed)
-//         {
-//             rb2d.AddForce(new Vector2(0, jumpForce));
-//             isJumpPressed = false;
-//             ChangeAnimationState(PLAYER_JUMP);
-//             Debug.Log("nhay ne");
-//         }
-//         else if (isAttackPressed)
-//         {
-//             ChangeAnimationState(PLAYER_ATTACK);
-//             isAttacking = true;
-//             Invoke("AttackComplete", attackDelay);
-//         }
-//         else if (Mathf.Abs(xAxis) > 0.1f)
-//         {
-//             ChangeAnimationState(PLAYER_RUN);
-//         }
-//         else
-//         {
-//             ChangeAnimationState(PLAYER_IDLE);
-//         }
-//     }
-
-//     void FixedUpdate()
-//     {
-//         float xAxis = Input.GetAxisRaw("Horizontal");
-//         Vector2 vel = new Vector2(xAxis * walkSpeed, rb2d.velocity.y);
-//         rb2d.velocity = vel;
-//     }
-
-//     void AttackComplete()
-//     {
-//         isAttacking = false;
-//     }
-
-//     void ChangeAnimationState(string newAnimation)
-//     {
-//         if (animator.GetCurrentAnimatorStateInfo(0).IsName(newAnimation)) return;
-//         animator.Play(newAnimation);
-//     }
-// }
-
-
-
-
-// [SerializeField]
-// private float walkSpeed = 5f;
-
-// private Animator animator;
-
-// private float xAxis;
-// private float yAxis;
-// private Rigidbody2D rb2d;
-// private bool isJumpPressed;
-// private float jumpForce = 600;
-// private int groundMask;
-// private bool isGrounded;
-// private string currentAnimaton;
-// private bool isAttackPressed;
-// private bool isAttacking;
-
-// [SerializeField]
-// private float attackDelay = 0.3f;
-
-// //Animation States
-// const string PLAYER_IDLE = "Player_Idle";
-// const string PLAYER_RUN = "Player_Run";
-// const string PLAYER_JUMP = "Player_Jumping";
-// const string PLAYER_ATTACK = "Player_Attack";
-
-// //=====================================================
-// // Start is called before the first frame update
-// //=====================================================
-// void Start()
-// {
-//     rb2d = GetComponent<Rigidbody2D>();
-//     animator = GetComponent<Animator>();
-//     groundMask = 1 << LayerMask.NameToLayer("Ground");
-
-// }
-
-// //=====================================================
-// // Update is called once per frame
-// //=====================================================
-// void Update()
-// {
-//     //Checking for inputs
-//     xAxis = Input.GetAxisRaw("Horizontal");
-
-//     //space jump key pressed?
-//     if (Input.GetKeyDown(KeyCode.Space))
-//     {
-//         isJumpPressed = true;
-//     }
-
-//     //space Atatck key pressed?
-//     if (Input.GetKeyDown(KeyCode.RightControl))
-//     {
-//         isAttackPressed = true;
-//     }
-// }
-
-// //=====================================================
-// // Physics based time step loop
-// //=====================================================
-// private void FixedUpdate()
-// {
-//     //check if player is on the ground
-//     RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundMask);
-
-//     if (hit.collider != null)
-//     {
-//         isGrounded = true;
-//     }
-//     else
-//     {
-//         isGrounded = false;
-//     }
-
-//     //------------------------------------------
-
-//     //Check update movement based on input
-//     Vector2 vel = new Vector2(0, rb2d.velocity.y);
-
-//     if (xAxis < 0)
-//     {
-//         vel.x = -walkSpeed;
-//         transform.localScale = new Vector2(-1, 1);
-//     }
-//     else if (xAxis > 0)
-//     {
-//         vel.x = walkSpeed;
-//         transform.localScale = new Vector2(1, 1);
-
-//     }
-//     else
-//     {
-//         vel.x = 0;
-
-//     }
-
-//     if (isGrounded && !isAttacking)
-//     {
-//         if (xAxis != 0)
-//         {
-//             ChangeAnimationState(PLAYER_RUN);
-//         }
-//         else
-//         {
-//             ChangeAnimationState(PLAYER_IDLE);
-//         }
-//     }
-//     //------------------------------------------
-
-//     //Check if trying to jump 
-//     if (isJumpPressed && isGrounded)
-//     {
-//         rb2d.AddForce(new Vector2(0, jumpForce));
-//         isJumpPressed = false;
-//         ChangeAnimationState(PLAYER_JUMP);
-//     }
-
-//     //assign the new velocity to the rigidbody
-//     rb2d.velocity = vel;
-
-
-//     //attack
-//     if (isAttackPressed)
-//     {
-//         isAttackPressed = false;
-
-//         if (!isAttacking)
-//         {
-//             isAttacking = true;
-
-//             if (isGrounded)
-//             {
-//                 ChangeAnimationState(PLAYER_ATTACK);
-//             }
-//             // else
-//             // {
-//             //     ChangeAnimationState(PLAYER_AIR_ATTACK);
-//             // }
-
-//             Invoke("AttackComplete", attackDelay);
-//         }
-//     }
-// }
-
-// void AttackComplete()
-// {
-//     isAttacking = false;
-// }
-
-// //=====================================================
-// // mini animation manager
-// //=====================================================
-// void ChangeAnimationState(string newAnimation)
-// {
-//     if (currentAnimaton == newAnimation) return;
-
-//     animator.Play(newAnimation);
-//     currentAnimaton = newAnimation;
-// }
